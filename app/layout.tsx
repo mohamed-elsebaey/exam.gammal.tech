52264d9446f00c0b64617550f65d5dc82761f9ca
Mohamed <elsebaey95@outlook.com>
2024-10-31 14:57:39 +0300

    V 0.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/footer/Footer";
-// import Header from "@/components/header/Header";
+// import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
 
 const open_Sans = Open_Sans({ subsets: ["latin"] });
 
@@ -24,18 +24,17 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
     <html lang="en" className="scroll-smooth">
       <body
         className={`${open_Sans.className} antialiased mx-auto max-w-[1800px]`}
       >
-        {/* <Header
+        <Header
           login={false}
           profilePath="https://res.cloudinary.com/dyryptpqq/image/upload/v1729810401/AlphaHerbs-Images/usersProfileImages/alpha-herbs.png"
-        /> */}
+        />
         <div className="mt-16">{children}</div>
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
