e010774b9f580bfcbafee67443c08b59700156f5
Mohamed <elsebaey95@outlook.com>
2024-10-31 13:49:56 +0300

    exam.gammal.tech V0.0

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,18 @@
+// import Hero from "@/components/hero/Hero";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+  description:
+    "Join the Gammal Tech community and sign in to unlock more features. ",
+};
+
+async function page() {
+  return (
+    <>
+      {/* <Hero /> */}
+    </>
+  );
+}
+
+export default page;
