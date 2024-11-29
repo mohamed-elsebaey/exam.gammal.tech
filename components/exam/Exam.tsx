71d7a2ed29468bfafe2625498192071d8aa77a33
Mohamed <elsebaey95@outlook.com>
2024-11-30 00:49:16 +0200

    ...

--- components/exam/Exam.tsx
+++ components/exam/Exam.tsx
@@ -12,36 +12,22 @@ interface ExamType {
 function Exam({
   examQuestions,
   onClick,
+  count,
 }: {
   examQuestions: ExamType;
   onClick: any;
+  count: any;
 }) {
-  const [count, setCount] = useState(5);
 
   const questionNumberInQuiz: string = examQuestions.id;
   const questionHeader: string = examQuestions.question;
   const Code: string = examQuestions.text;
   const options: string[] = examQuestions.options;
 
-  // ********************************* *** Timer *** *********************************
-  const intervalRef: any = useRef<NodeJS.Timeout | null>(null);
-  
-  useEffect(() => {
-    setCount(5);
-    intervalRef.current = setInterval(() => {
-      if (count != 0) {
-        console.log(count)
-        setCount((prevCount) => prevCount - 1);
-      }
-    }, 1000);
-    return () => clearInterval(intervalRef.current);
-  }, [examQuestions]);
-
-  // ********************************* *** ***** *** *********************************
 
   return (
     <div className="container flex gap-4 flex-wrap justify-center items-start">
-      <div className="w-[100%] lg:grow lg:max-w-[65%]">
+      <div className="w-[100%] lg:grow lg:max-w-[65%] ">
         <CodeBlock language="javascript" value={Code} />
       </div>
       <div className="relative w-[100%] overflow-auto sm:w-[85%] md:w-[70%] lg:w-96 bg-[#282c34] p-4 rounded-md select-none text-white h-[532px] max-h-[532px] text-center flex flex-col gap-4">
@@ -63,7 +49,11 @@ function Exam({
               className="bg-white text-primary py-4 px-2 rounded-lg cursor-pointer"
               key={index + 1}
               onClick={() =>
-                onClick({ id: questionNumberInQuiz, value: option , time: 15-count })
+                onClick({
+                  id: questionNumberInQuiz,
+                  value: option,
+                  time:  count,
+                })
               }
             >
               {option}
--- components/exam/ExamsController.tsx
+++ components/exam/ExamsController.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Exam from "./Exam";
 import { examCorrection } from "@/app/exam/_action/examCorrection";
 
@@ -14,19 +14,45 @@ function ExamsController({ quizData }: { quizData: quizData[] }) {
   const [questionNumber, setQuestionNumber] = useState(0);
   const [answers, setAnswers] = useState<string[]>([]);
 
+  const duration = 15;
+  const [count, setCount] = useState(duration);
+
   const questions = quizData;
 
-  const examsAnswers = (answer: string) => {
+  function examsAnswers(answer: string) {
     if (questions.length - 1 > questionNumber) {
       setAnswers([...answers, answer]);
       setQuestionNumber((questionNumber) => questionNumber + 1);
     } else {
+      // setQuestionNumber(0);
       examCorrection([...answers, answer]);
     }
-  };
+  }
+
+  // ********************************* *** Timer Start *** *********************************
+  useEffect(() => {
+    setCount(duration);
+    const intervalId = setInterval(() => {
+      setCount((prevCount) => {
+        if (prevCount === 1) {
+          clearInterval(intervalId);
+          // setQuestionNumber(() => questionNumber + 1);
+          examsAnswers("");
+        }
+        return prevCount ? prevCount - 1 : 0;
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [questionNumber]);
+  // ********************************* *** Timer  End  *** *********************************
 
   return (
-    <Exam examQuestions={questions[questionNumber]} onClick={examsAnswers} />
+    <Exam
+      examQuestions={questions[questionNumber]}
+      onClick={examsAnswers}
+      count={count}
+    />
   );
 }
 
