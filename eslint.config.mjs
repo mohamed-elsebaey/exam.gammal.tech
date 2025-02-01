84af2bd2d132cf46b94f105113764baf855189bf
Mohamed <elsebaey95@outlook.com>
2025-02-02 01:01:26 +0200

    "@typescript-eslint/no-unused-expressions": "off",

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       // Disabled type 'any' Error
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
     },
   },
 ];
